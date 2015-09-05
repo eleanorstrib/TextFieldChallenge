38271ae988472e0ada86edffc8f38f476cc3212e
eleanorstrib <eleanor@sloan.mit.edu>
2015-09-05 16:29:22 -0700

    All working except I need to write the currency value to the view

--- TextFields/CurrencyDelegate.swift
+++ TextFields/CurrencyDelegate.swift
@@ -14,17 +14,31 @@ class CurrencyTextFieldDelegate: NSObject, UITextFieldDelegate {
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
-        //trying out an approach that value will be set to 0,
-        //convert string to number display converted
-
-        let newText : Int? = textField.text.toInt()
-        var formatter = NSNumberFormatter()
-        formatter.numberStyle = .CurrencyStyle
+        var currencyRange: NSRange!
         
-        formatter.stringFromNumber(newText!)
-        println(newText)
-        println(newText.dynamicType)
+        // set a variable for the current and updated text
+        var currentText = textField.text as NSString
+        currentText = currentText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        //Invoke instance of NSNumberFormatter
+        //convert string to a float
+        let formatter = NSNumberFormatter()
+        let currencyInt = formatter.numberFromString(currentText as String)
+        let currencyFloat = (currencyInt!.floatValue)/100
+
+        //Use currency style method to convert our float 
+        //to a currency formatted string
+        formatter.numberStyle = .CurrencyStyle
+        formatter.locale = NSLocale.currentLocale()
+        var currencyFormatted: NSString
+        currencyFormatted = formatter.stringFromNumber(currencyFloat)!
+        
+        //print statements to debug
+        println(currencyFormatted)
+        println(currencyFormatted.dynamicType)
+
+
         return true
-        //replace 0 with numbers and show format as currency
+
     }
 }
