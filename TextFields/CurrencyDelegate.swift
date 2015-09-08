780734c43245ad9c2de9241bbed84c63ad2f2894
eleanorstrib <eleanor@sloan.mit.edu>
2015-09-08 12:18:05 -0700

    minor changes to currency delegate file, still not working to spec

--- TextFields/CurrencyDelegate.swift
+++ TextFields/CurrencyDelegate.swift
@@ -14,11 +14,12 @@ class CurrencyTextFieldDelegate: NSObject, UITextFieldDelegate {
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
-        var currencyRange: NSRange!
+        var currencyRange: NSRange
         
         // set a variable for the current and updated text
         var currentText = textField.text as NSString
         currentText = currentText.stringByReplacingCharactersInRange(range, withString: string)
+        currencyRange = currentText.rangeOfString(currentText as String)
         
         //Invoke instance of NSNumberFormatter
         //convert string to a float
@@ -26,17 +27,22 @@ class CurrencyTextFieldDelegate: NSObject, UITextFieldDelegate {
         let currencyInt = formatter.numberFromString(currentText as String)
         let currencyFloat = (currencyInt!.floatValue)/100
 
-        //Use currency style method to convert our float 
+        //Use currency style method to convert our float
         //to a currency formatted string
         formatter.numberStyle = .CurrencyStyle
         formatter.locale = NSLocale.currentLocale()
-        var currencyFormatted: NSString
-        currencyFormatted = formatter.stringFromNumber(currencyFloat)!
+
+        var currencyFormatted = String()
         
+        currencyFormatted = formatter.stringFromNumber(currencyFloat)!
+
+        currentText = currentText.stringByReplacingCharactersInRange(currencyRange, withString: currencyFormatted)
+
         //print statements to debug
         println(currencyFormatted)
         println(currencyFormatted.dynamicType)
-
+        println(currentText as String)
+        println(currentText.dynamicType)
 
         return true
 
